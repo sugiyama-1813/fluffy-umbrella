730e7ab249cccc49cb1a4f1f4b279e4f53218ee8
sugiyama-1813 <zm26090@josai.ac.jp>
2026-07-06 23:11:18 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function desk(){
+  document.getElementById("message").innerHTML=
+    "メモを見つけた！「図書館を調べろ」";
+}
+
+function locker(){
+  document.getElementById("message").innerHTML=
+    "ロッカーから鍵を見つけた！";
+}
+
+function board(){
+  document.getElementById("message").innerHTML=
+    "黒板には「本棚を探せ」と書かれている。";
+}
